46002e8dca92669ab5b22ad3e81372ef31572349
AhmedTalat99 <ahmedmohamed19_sd@fcis.bsu.edu.eg>
2024-05-17 18:36:20 +0300

    create AppStyle

---
+++ lib/utils/app_styles.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+abstract class AppStyle {
+  static const TextStyle styleRegular16 = TextStyle(
+    color: Color(0xff064060),
+    fontSize: 16,
+    fontFamily: 'Montserrat',
+    fontWeight: FontWeight.w400,
+  );
+  static const TextStyle styleBold16 = TextStyle(
+    color: Color(0xff4EB7F2),
+    fontSize: 16,
+    fontFamily: 'Montserrat',
+    fontWeight: FontWeight.w700,
+  );
+  static const TextStyle styleMedium16 = TextStyle(
+    color: Color(0xff064061),
+    fontSize: 16,
+    fontFamily: 'Montserrat',
+    fontWeight: FontWeight.w500,
+  );
+  static const TextStyle styleMedium20 = TextStyle(
+    color: Color(0xffffffff),
+    fontSize: 20,
+    fontFamily: 'Montserrat',
+    fontWeight: FontWeight.w500,
+  );
+  static const TextStyle styleSemiBold16 = TextStyle(
+    color: Color(0xff064061),
+    fontSize: 16,
+    fontFamily: 'Montserrat',
+    fontWeight: FontWeight.w600,
+  );
+  static const TextStyle styleSemiBold20 = TextStyle(
+    color: Color(0xff064061),
+    fontSize: 20,
+    fontFamily: 'Montserrat',
+    fontWeight: FontWeight.w600,
+  );
+  static const TextStyle styleRegular12 = TextStyle(
+    color: Color(0xffAAAAAA),
+    fontSize: 12,
+    fontFamily: 'Montserrat',
+    fontWeight: FontWeight.w400,
+  );
+}
